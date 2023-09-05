4775227215527090c2037b4fe037250c444b8b65
Jamin M <mc.jamin007@gmail.com>
2023-09-05 12:14:23 +0000

    Update index.html and App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import React, { useState } from 'react';
 import "./styles.css";
+
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Count App</h1>
+      <h2>Press to start Counting</h2>
+      <Counting/>
     </div>
+  );
+}
+
+function Counting(){
+  const [count, setCount] = useState(0);
+  const increase = () => setCount(count+1)
+  return (
+    <>
+    <p> You have clicked {count} times. </p>
+    <button onClick={increase}> Click ++ </button>
+    </>
   );
 }
