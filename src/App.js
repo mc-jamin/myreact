8f9c615034d452cc0ed08988a72382a10d815575
Jamin M <mc.jamin007@gmail.com>
2023-09-14 11:19:33 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,25 +9,49 @@ const App=(props)=>{
     </div>
   );
 }
-const Counting=(props)=> {
+const Counting=()=> {
   const [count, setCount] = useState(0);
   const [isPositive, setPositive] = useState(true);
-  const increase =()=>{
-    setPositive(true);
-    setCount(count+1);
-  }
-  const decrease =()=>{
-    if(count>0) setCount(count-1);
-    else setPositive(false);    
-  }  
+  const buttonValue = {
+   buttonAdd : {
+    type : "button",
+    name : "addition",
+    value : "Add",
+    class : "btn",
+    onclick : ()=>{
+      setPositive(true);
+      setCount(count+1);
+    }
+   },
+   buttonSubtract : {
+    type : "button",
+    name : "subtraction",
+    value : "Subtract",
+    class : "btn",
+    onclick : ()=>{
+      if(count>0) setCount(count-1);
+      else setPositive(false);
+    }
+   }
+  };
   return (
-    <>  
-    <input type ="button" className = "btn" onClick={increase} value = "Add"/>
-    <p><input type = "button" className = "btn" onClick={decrease} value = "Subtract"/></p>
+    <div> 
+    <p> 
+    <input type ={buttonValue.buttonAdd.type} 
+          className = {buttonValue.buttonAdd.class} 
+          onClick={buttonValue.buttonAdd.onclick} 
+          value = {buttonValue.buttonAdd.value}/>
+    </p>
+    <p>
+      <input type ={buttonValue.buttonSubtract.type} 
+            className = {buttonValue.buttonSubtract.class} 
+            onClick={buttonValue.buttonSubtract.onclick} 
+            value = {buttonValue.buttonSubtract.value}/>
+    </p>
     <p>
       { isPositive ? `Clicked ${count} times`: "Num cannot be negative" }
     </p>
-    </>
+    </div>
   );
 }
 export default App;
