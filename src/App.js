098c519ecbf09a36c8c5caf7838483fd3d87399e
Jamin M <mc.jamin007@gmail.com>
2023-09-19 12:10:34 +0000

    styling with bootstrap

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import React, { useState } from 'react';
 import "./styles.css";
 const App=(props)=>{
   return (
-    <div className="App">
-      <h1>Count App</h1>
-      <h2>Press to start Counting</h2>
-      <Counting/>
+    <div className ="container-sm m-2 p-2">
+      <div className="row">
+        <div className = "col"></div>
+          <div className="col-8">
+            <div className="m-2 p-2">
+              <Counting/>
+            </div>
+          </div>
+      <div className="col"></div>
+      </div>
     </div>
   );
 }
@@ -17,7 +23,6 @@ const Counting=()=> {
     type : "button",
     name : "addition",
     value : "Add",
-    class : "btn",
     onclick : ()=>{
       setPositive(true);
       setCount(count+1);
@@ -27,7 +32,6 @@ const Counting=()=> {
     type : "button",
     name : "subtraction",
     value : "Subtract",
-    class : "btn",
     onclick : ()=>{
       if(count>0) setCount(count-1);
       else setPositive(false);
@@ -35,23 +39,31 @@ const Counting=()=> {
    }
   };
   return (
-    <div> 
-    <p> 
-    <input type ={buttonValue.buttonAdd.type} 
-          className = {buttonValue.buttonAdd.class} 
-          onClick={buttonValue.buttonAdd.onclick} 
-          value = {buttonValue.buttonAdd.value}/>
-    </p>
-    <p>
-      <input type ={buttonValue.buttonSubtract.type} 
-            className = {buttonValue.buttonSubtract.class} 
+    <>
+    <div class="bg-light card "> 
+      <h1 className="card-title card-header">Counting App</h1>
+      <div className="container">
+        <h2 className=" d-flex bd-highlight justify-content-centre card-text mt-4 p-2">Press to start Counting</h2>
+        <div className="d-flex justify-content flex-column">
+          <p> 
+            <input type ={buttonValue.buttonAdd.type} 
+            className = "btn bg-primary text-white m-2" 
+            onClick={buttonValue.buttonAdd.onclick} 
+            value = {buttonValue.buttonAdd.value}/>
+          </p>
+          <p>
+            <input type ={buttonValue.buttonSubtract.type} 
+            className = "btn bg-primary text-white m-2"
             onClick={buttonValue.buttonSubtract.onclick} 
             value = {buttonValue.buttonSubtract.value}/>
-    </p>
-    <p>
-      { isPositive ? `Clicked ${count} times`: "Num cannot be negative" }
-    </p>
-    </div>
+          </p>
+        </div>
+          <p className="text-muted"> 
+            { isPositive ? `Clicked ${count} times`: "Num cannot be negative" }
+          </p>
+      </div>      
+    </div>    
+  </>
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
