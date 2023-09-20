26a25bed8ed45178efaaa38921cbcf04f264d8af
Jamin M <mc.jamin007@gmail.com>
2023-09-20 07:42:19 +0000

    update to UI and funnctionality

--- src/Counting.jsx
+++ src/Counting.jsx
@@ -23,41 +23,39 @@ const Counting = () => {
     },
   };
   return (
-    <>
-      <div class="bg-light card ">
-        <h1 className="card-title card-header">Counting App</h1>
-        <div className="container">
-          <h2 className=" d-flex bd-highlight justify-content-centre card-text mt-4 p-2">
-            Press to start Counting
-          </h2>
-          <div className="d-flex justify-content flex-column">
-            <p>
-              <input
-                type={buttonValue.buttonAdd.type}
-                className="btn bg-primary text-white m-2"
-                onClick={buttonValue.buttonAdd.onclick}
-                value={buttonValue.buttonAdd.value}
-              />
-            </p>
-            <p>
-              <input
-                type={buttonValue.buttonSubtract.type}
-                className="btn bg-primary text-white m-2"
-                onClick={buttonValue.buttonSubtract.onclick}
-                value={buttonValue.buttonSubtract.value}
-              />
-            </p>
-          </div>
-          <p className="text-muted">
-            {isPositive
-              ? count === 1
-                ? `Clicked ${count} time`
-                : `Clicked ${count} times`
-              : "Num cannot be negative"}
+    <div class="bg-light card ">
+      <h1 className="card-title card-header">Counting App</h1>
+      <div className="container">
+        <h2 className=" d-flex bd-highlight justify-content-centre card-text mt-4 p-2">
+          Press to start Counting
+        </h2>
+        <div className="d-flex justify-content flex-column">
+          <p>
+            <input
+              type={buttonValue.buttonAdd.type}
+              className="btn bg-primary text-white m-2"
+              onClick={buttonValue.buttonAdd.onclick}
+              value={buttonValue.buttonAdd.value}
+            />
+          </p>
+          <p>
+            <input
+              type={buttonValue.buttonSubtract.type}
+              className="btn bg-primary text-white m-2"
+              onClick={buttonValue.buttonSubtract.onclick}
+              value={buttonValue.buttonSubtract.value}
+            />
           </p>
         </div>
+        <p className="text-muted">
+          {isPositive
+            ? count === 1
+              ? `Clicked ${count} time`
+              : `Clicked ${count} times`
+            : "Num cannot be negative"}
+        </p>
       </div>
-    </>
+    </div>
   );
 };
 export default Counting;
--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
